15893d728483a5a5cd18412918080dd618a4fed1
Karina Sotz <karisimon23@gmail.com>
2020-11-27 16:42:14 -0600

    mysql

--- partials/db.php
+++ partials/db.php
@@ -2,7 +2,7 @@
     $conn = mysqli_connect(
         'sql214.main-hosting.eu',
         'u688267941_diacogt',
-        'u688267941_diacogt'
         'Minnie11.'
+        'u688267941_diacogt'
     );
 ?>
